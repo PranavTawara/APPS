15e4a4c2c793cbea76bf5d40c8c7aecf63e3e68c
Pranav <pranavmarch12@gmail.com>
2024-07-19 20:35:24 +0530

    updated

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
-const express = require('express');
+require('dotenv').config();
+
 const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const promBundle = require('express-prom-bundle');
+
 const app = express();
-const router = require('./router/router'); // Assuming this contains your CRUD routes
-// Middleware to parse JSON bodies
-app.use(express.json());
+const port = process.env.PORT || 5000;
 
-// Enable CORS
+mongoose.connect(process.env.Mongo_url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Failed to connect to MongoDB:', err));
+
+app.use(bodyParser.json());
 app.use(cors());
+app.use(promBundle({ includeMethod: true }));
 
-// Connect to MongoDB
-const mongoURI = 'mongodb://localhost:27017/claims-management';
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch(err => console.error('Failed to connect to MongoDB', err));
+const { Policyholder, Policy, Claim } = require('./models/models');
+const router = require('./router/router');
 
-// Mount the router at '/api'
 app.use('/api', router);
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
